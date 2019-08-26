5c2f10d86055240afec34092223ab5bad4e5c638
Frédéric <f.lugbull@gmail.com>
2019-08-26 23:03:02 +0200

    1er commit

---
+++ script.js
@@ -0,0 +1,19 @@
+
+// // Fonctionnalité 1 :
+// // On commence par un petit échauffement : lorsque
+//  l'utilisateur va cliquer sur le footer (portant le 
+//   tag <footer>), tu vas afficher le mot "clique" en console.
+
+// // Cette fonctionnalité doit être codée avec un 
+// addEventListener("click", function(){ } car c'est 
+// une bonne habitude à prendre ! 😇
+
+// // Fonctionnalité 1-bis :
+// // Maintenant on va upgrader cette première 
+// fonctionnalité : lorsque l'utilisateur va cliquer
+//  sur le footer, tu vas afficher en console "clic numéro x" 
+//  avec x qui commence à 1 et s'incrémente de +1 à chaque clic.
+
+
+var
+addEventListener("click", function(){ })
